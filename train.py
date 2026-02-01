9426162a46afff24872350df921108323d86fcd9
hxfei <284310730@qq.com>
2026-02-01 21:17:03 +0800

    1.默认将数据集放在顶层目录中；2.调整train.py和val.py

--- train.py
+++ train.py
@@ -29,10 +29,10 @@ from ultralytics import YOLO
 if __name__ == '__main__':
     model = YOLO('ultralytics/cfg/models/11/yolo11n.yaml') # YOLO11
     # model.load('yolo11n.pt') # loading pretrain weights
-    model.train(data='/home/book/voc-ai-tod.yaml',
+    model.train(data='datasets/voc-ai-tod.yaml',
                 cache=False,
                 imgsz=640,
-                epochs=30,
+                epochs=2,
                 batch=8,
                 close_mosaic=0, # 最后多少个epoch关闭mosaic数据增强，设置0代表全程开启mosaic训练
                 workers=4, # Windows下出现莫名其妙卡主的情况可以尝试把workers设置为0
@@ -42,7 +42,7 @@ if __name__ == '__main__':
                 # resume=True, # 断点续训,YOLO初始化时选择last.pt,不懂就在百度云.txt找断点续训的视频
                 # amp=False, # close amp | loss出现nan可以关闭amp
                 # fraction=0.2,
-                project='/home/book/1.experiment/result',
-                name='test',
+                project='runs/experiment',
+                name='yolo11n',
                 exist_ok=True,
                 )
