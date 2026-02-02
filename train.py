6271ed29949941725700ae07731b3ccbcf37b452
hxfei <284310730@qq.com>
2026-02-02 21:43:28 +0800

    yolov11n 基准模型 640 进行训练

--- train.py
+++ train.py
@@ -27,10 +27,10 @@ if __name__ == '__main__':
     model.train(data='datasets/voc-ai-tod.yaml',
                 cache=False,
                 imgsz=640,
-                epochs=2,
-                batch=8,
+                epochs=10000,
+                batch=16,
                 close_mosaic=0, # 最后多少个epoch关闭mosaic数据增强，设置0代表全程开启mosaic训练
-                workers=4, # Windows下出现莫名其妙卡主的情况可以尝试把workers设置为0
+                workers=8, # Windows下出现莫名其妙卡主的情况可以尝试把workers设置为0
                 # device='0,1', # 指定显卡和多卡训练参考<YOLOV11配置文件.md>下方常见错误和解决方案
                 optimizer='SGD', # using SGD
                 # patience=0, # set 0 to close earlystop.
