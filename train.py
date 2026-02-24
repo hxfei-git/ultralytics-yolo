83b76891a7b788efb7cb5396bfcf460072237ff0
root <autodl>
2026-02-24 12:44:55 +0800

    ACSPP kernel=3

--- train.py
+++ train.py
@@ -25,9 +25,9 @@ from ultralytics import YOLO
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Train YOLO11 with optional YAML and name")
     parser.add_argument('--yaml', type=str,
-                        default='ultralytics/cfg/models/11/yolo11-C3k2_CGFF-p3.yaml')
+                        default='ultralytics/cfg/models/11/yolo11-ACSPP.yaml')
     parser.add_argument('--name', type=str,
-                        default='yolo11-C3k2_CGFF-p3')
+                        default='yolo11n-ACSPP')
     args = parser.parse_args()
 
     model = YOLO(args.yaml)  # YOLO11
@@ -37,9 +37,9 @@ if __name__ == '__main__':
         cache=False,
         imgsz=640,
         epochs=10000,
-        batch=16,
+        batch=32,
         close_mosaic=0,  # 最后多少个epoch关闭mosaic数据增强，设置0代表全程开启mosaic训练
-        workers=8,       # Windows下出现莫名其妙卡主的情况可以尝试把workers设置为0
+        workers=12,       # Windows下出现莫名其妙卡主的情况可以尝试把workers设置为0
         # device='0,1',  # 指定显卡和多卡训练参考<YOLOV11配置文件.md>下方常见错误和解决方案
         optimizer='SGD',  # using SGD
         # patience=0,    # set 0 to close earlystop.
